ac19effa7f8a34a43c0277e3fa3fceb07b8d5c3d
yoo-chaewon <ohwow5@sookmyung.ac.kr>
2019-04-05 18:26:41 +0900

    return json

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,23 @@
 package com.example.demo.controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
     @RequestMapping("/hello")
     public @ResponseBody
+    /*
     String hello() {
         String greetings = "Hello";
         return greetings;
+    }*/
+    Map<String, String> hello(){
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "111");
+        map.put("age", "2222");
+        return map;
     }
 }
